a68bf65366f6098aac79475e7e33dff740bf9db7
Jason Waldrip <jason@waldrip.net>
2012-10-08 09:29:20 -0600

    Bump to 1.0.1

---
+++ spec/presenter_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe SampleObjectPresenter do
+
+  it '.presenter_method (full_name)' do
+    object = FactoryGirl.create(:sample_object)
+    presented_object = SampleObjectPresenter.new(object)
+    expect(presented_object.full_name).to be_present
+  end
+
+  it '.model_instance_method (first_name)' do
+    object = FactoryGirl.create(:sample_object)
+    presented_object = SampleObjectPresenter.new(object)
+    expect(presented_object.first_name).to be_present
+  end
+
+  it '#hide_method' do
+    SampleObjectPresenter.send :hide, :id
+    object = FactoryGirl.create(:sample_object)
+    presented_object = SampleObjectPresenter.new(object)
+    require 'pry'
+    binding.pry
+    expect { presented_object.id }.to raise_error(DelegatedPresenter::Error::MethodHidden)
+  end
+
+  it '#expose_method' do
+    SampleObjectPresenter.send :expose, :first_name
+    object = FactoryGirl.create(:sample_object)
+    presented_object = SampleObjectPresenter.new(object)
+    require 'pry'
+    binding.pry
+    expect { presented_object.id }.to raise_error(DelegatedPresenter::Error::MethodNotExposed)
+    expect(presented_object.first_name).to be_present
+  end
+
+end
