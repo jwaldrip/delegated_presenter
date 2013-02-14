57c7d57128091e1e0bda2103a5fa5806d393168b
Jason Waldrip <jason@waldrip.net>
2013-02-14 13:21:36 -0700

    refactor

--- lib/delegated_presenter/base.rb
+++ lib/delegated_presenter/base.rb
@@ -5,15 +5,77 @@
 #    If a method is not exposed.
 class DelegatedPresenter::Base < SimpleDelegator
 
-  PRESENTABLE = {}
-  HIDDEN_METHODS = {}
-  EXPOSED_METHODS = {}
-
-  private_constant :PRESENTABLE, :HIDDEN_METHODS, :EXPOSED_METHODS
-
-  delegate :exposed_methods, :hidden_methods, :presentable_objects, to: :presenter_class
+  delegate :exposed_methods, :hidden_methods, :presentable, to: :presenter_class
 
   alias :presented_model :__getobj__
+
+  class << self
+
+    attr_writer :presentable, :hidden_methods, :exposed_methods
+
+    protected
+
+    # Presentable Objects
+    # @api private
+    def presentable
+      @presentable ||= [] unless name == 'DelegatedPresenter::Base'
+    end
+
+    # Methods to hide
+    # @api private
+    def hidden_methods
+      @hidden_methods ||= [] unless name == 'DelegatedPresenter::Base'
+    end
+
+    # Methods to expose
+    # @api private
+    def exposed_methods
+      @exposed_methods ||= [] unless name == 'DelegatedPresenter::Base'
+    end
+
+    private
+
+    # Determine what objects what to present.
+    # @!visibility public
+    # @param :objects One or more models to be presented.
+    # @example Add some presentable objects:
+    #     presents :contact, :user, :client
+    def presents(*objects)
+      self.presentable += objects.flatten.collect{ |i| i.to_s.camelize.singularize }
+    end
+
+    # @!visibility public
+    # Hide methods from the presenter.
+    # @param methods one ore more methods from the model to hide from the presenter.
+    # @example Hide methods:
+    #     hide :id, :crypted_password, :password_salt
+    def hide(*hidden_methods)
+      self.hidden_methods += hidden_methods
+    end
+
+    # @!visibility public
+    # Exposes methods to the presenter, when defined all others will be hidden.
+    # @param :methods one ore more methods from the model to expose from the presenter.
+    # @example Expose methods:
+    #     expose :first_name, :last_name
+    def expose(*exposed_methods)
+      self.exposed_methods += exposed_methods.flatten
+    end
+
+    ## Internal Methods: Not Documented
+
+    # @api private
+    def inherited(subclass)
+      subclass.instance_variable_set :@presentable, @presentable
+      subclass.instance_variable_set :@hidden_methods, @hidden_methods
+      subclass.instance_variable_set :@exposed_methods, @exposed_methods
+      return unless subclass.name.present?
+      presentable_class = subclass.name.gsub(/Presenter$/,'')
+      presentable_class.constantize rescue nil
+      subclass.send :presents, presentable_class if Object.const_defined?(presentable_class)
+    end
+
+  end
 
   # Initializes the presenter with an object.
   # @param object Can be an collection or instance of a presentable models.
@@ -25,9 +87,9 @@ class DelegatedPresenter::Base < SimpleDelegator
     else
       raise DelegatedPresenter::Error::NotPresentable, "#{self.presenter_class} cannot present a #{object.class}" unless object_is_presentable?(object)
       __setobj__(object)
-      if exposed_methods
+      if exposed_methods.present?
         expose_methods
-      elsif hidden_methods
+      elsif hidden_methods.present?
         hide_methods
       end
     end
@@ -42,65 +104,6 @@ class DelegatedPresenter::Base < SimpleDelegator
   end
 
   private
-
-  # Determine what objects what to present.
-  # @!visibility public
-  # @param :objects One or more models to be presented.
-  # @example Add some presentable objects:
-  #     presents :contact, :user, :client
-  def self.presents(*objects)
-    PRESENTABLE[name] ||= []
-    PRESENTABLE[name] += objects.flatten.collect{ |i| i.to_s.camelize.singularize }
-  end
-
-  # @!visibility public
-  # Hide methods from the presenter.
-  # @param methods one ore more methods from the model to hide from the presenter.
-  # @example Hide methods:
-  #     hide :id, :crypted_password, :password_salt
-  def self.hide(*hidden_methods)
-    self.hidden_methods[name] ||= []
-    self.hidden_methods[name] += hidden_methods
-  end
-
-  # @!visibility public
-  # Exposes methods to the presenter, when defined all others will be hidden.
-  # @param :methods one ore more methods from the model to expose from the presenter.
-  # @example Expose methods:
-  #     expose :first_name, :last_name
-  def self.expose(*exposed_methods)
-    EXPOSED_METHODS[name] ||= []
-    EXPOSED_METHODS[name] += exposed_methods.flatten
-  end
-
-  ## Internal Methods: Not Documented
-
-  # @api private
-  def self.inherited(subclass)
-    presentable_class = subclass.name.gsub(/Presenter$/,'')
-    presentable_class.constantize rescue nil
-    subclass.presents presentable_class if Object.const_defined?(presentable_class)
-  end
-
-  ## Inherited Object Getters
-
-  # Presentable Objects
-  # @api private
-  def self.presentable_objects
-    (PRESENTABLE[name] || [])
-  end
-
-  # Methods to expose
-  # @api private
-  def self.exposed_methods
-    EXPOSED_METHODS[name]
-  end
-
-  # Methods to hide
-  # @api private
-  def self.hidden_methods
-    HIDDEN_METHODS[name]
-  end
 
   # Raises an error when hidden methods are called.
   # @api private
@@ -125,7 +128,7 @@ class DelegatedPresenter::Base < SimpleDelegator
   # Determines if an object is presentable
   # @api private
   def object_is_presentable?(object)
-    presentable_objects.include?(object.class.name)
+    presentable.include?(object.class.name)
   end
 
   # Extracts the the models unique methods from the superclass
