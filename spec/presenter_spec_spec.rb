7063e8e6571f6a99a769f14edf19e01d01572246
Jason Waldrip <jason@waldrip.net>
2013-02-14 13:20:19 -0700

    Test without rails and get to 100% coverage

--- spec/presenter_spec_spec.rb
+++ spec/presenter_spec_spec.rb
@@ -1,17 +1,96 @@
 require 'spec_helper'
 
-describe SampleObjectPresenter do
+describe DelegatedPresenter::Base do
 
-  it '.presenter_method (full_name)' do
-    object = FactoryGirl.create(:sample_object)
-    presented_object = SampleObjectPresenter.new(object)
-    expect(presented_object.full_name).to be_present
+  let(:object_class) do
+    object_class = Class.new do
+
+      def foo
+        "foo"
+      end
+
+      def bar
+        "bar"
+      end
+
+      def baz
+        "baz"
+      end
+
+    end
+
+    stub_const 'Foo', object_class
+  end
+  let(:object_instance) { object_class.new }
+  let(:presenter) do
+    object_class
+    presenter = Class.new(DelegatedPresenter::Base) do
+      presents :foo
+    end
+    stub_const 'FooPresenter', presenter
+  end
+  let(:presented_object){ presenter.new(object_instance) }
+
+  describe '.hide' do
+    let(:presenter) do
+      object_class
+      presenter = Class.new(DelegatedPresenter::Base) do
+        presents :foo
+        hide :foo
+      end
+      stub_const 'FooPresenter', presenter
+    end
+    context 'when a hidden method is called' do
+      it 'should raise an error' do
+        expect { presented_object.foo }.to raise_error DelegatedPresenter::Error::MethodHidden
+      end
+    end
+
+    context 'when a non hidden method is called' do
+      it 'should raise an error' do
+        expect { presented_object.bar }.to_not raise_error DelegatedPresenter::Error::MethodHidden
+      end
+    end
   end
 
-  it '.model_instance_method (first_name)' do
-      object = FactoryGirl.create(:sample_object)
-      presented_object = SampleObjectPresenter.new(object)
-      expect(presented_object.first_name).to be_present
+  describe '.expose' do
+    let(:presenter) do
+      object_class
+      presenter = Class.new(DelegatedPresenter::Base) do
+        presents :foo
+        expose :foo
+      end
+      stub_const 'FooPresenter', presenter
     end
+    context 'when a exposed method is called' do
+      it 'should raise an error' do
+        expect { presented_object.foo }.to_not raise_error DelegatedPresenter::Error::MethodNotExposed
+      end
+    end
+
+    context 'when a non exposed method is called' do
+      it 'should raise an error' do
+        expect { presented_object.bar }.to raise_error DelegatedPresenter::Error::MethodNotExposed
+      end
+    end
+  end
+
+  describe '#class' do
+    it "should be the model class" do
+      presented_object.class.should == object_class
+    end
+  end
+
+  describe '#model_instance_method (first_name)' do
+    it 'Calls a method on the presenter' do
+      presented_object.foo.should be_present
+    end
+  end
+
+  describe '#model_instance_method (first_name)' do
+    it 'calls an instance in the model' do
+      presented_object.foo.should be_present
+    end
+  end
 
 end
