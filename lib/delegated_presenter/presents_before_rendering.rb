530b9438ba14347ff203a73bcda4f1aa158bb96e
Jason Waldrip <jason@waldrip.net>
2013-02-14 13:21:27 -0700

    Added options

--- lib/delegated_presenter/presents_before_rendering.rb
+++ lib/delegated_presenter/presents_before_rendering.rb
@@ -3,21 +3,23 @@ module DelegatedPresenter::PresentsBeforeRendering
 
   included do
     class_attribute :presents_before_rendering
-    self.presents_before_rendering = []
+    self.presents_before_rendering = {}
   end
 
   private
 
   # Presents specified instance variables before rendering.
   def render
-    presents_before_rendering.each do |var|
-      ivar = instance_variable_get("@#{var}")
-      if ivar.present?
-        object_class = [ivar].flatten.collect(&:class).first.to_s
-        instance_variable_set("@#{var}", (object_class + 'Presenter').constantize.new(ivar))
-      end
+    presents_before_rendering.each do |var, options|
+      next if (
+        options.has_key?(:only) && !Array.wrap(options[:only]).include?(action_name.to_sym)
+      ) || (
+        options.has_key?(:except) && Array.wrap(options[:except]).include?(action_name.to_sym)
+      ) || (ivar = instance_variable_get "@#{var}").blank?
+      object_class = [ivar].flatten.collect(&:class).first.to_s
+      presenter = options.fetch(:with, "#{object_class}Presenter").to_s.classify.constantize
+      instance_variable_set "@#{var}", presenter.new(ivar)
     end
-
     super
   end
 
@@ -34,9 +36,13 @@ module DelegatedPresenter::PresentsBeforeRendering
     #     Specifies which instance variables to present, assumes the presenter has a name of *InstanceClassPresenter*.
     #     @param instance_vars the instance variables to present.
     #     @option options [Symbol] :with The presenter to use.
+    #     @option options [Symbol] :only the only action to present on.
+    #     @option options [Symbol] :except these actions to present on.
     def presents(*instance_vars)
-      options = instance_vars.extract_options!
-      self.presents_before_rendering += instance_vars.flatten.map(&:to_s)
+      options                        = instance_vars.extract_options!
+      instance_vars.each do |var|
+        self.presents_before_rendering[var.to_sym] = options
+      end
     end
 
     alias :present :presents
